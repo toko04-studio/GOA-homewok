da1158c5840be33fef1d14fabdf142e97a8bc3e4
toko04-studio <tokotoko4781@gmail.com>
2026-07-06 15:09:35 +0400

    second commit

---
+++ level 003/cw003.py
@@ -0,0 +1,13 @@
+print(5 + 5)
+print(10 - 5)
+print(2 * 4)
+print(1 / 3)
+
+name="tornike"
+age=14
+color="black"
+car="alfa romeo"
+live="zestafoni"
+ 
+
+print("my name is" + name + "am " + age  "my favirute color is" + color + "my favorite car is" +  car + "im live in" + live + ".")
---
+++ level 003/hw003.py
@@ -0,0 +1,33 @@
+#კოდის წერიკს პროცესი შედგება 3 ნაწილისგან
+
+# 1) inpunt
+# 2) output
+# 3) procesing
+
+print(10 + 35)
+print(10 - 7)
+print(5 * 5)
+print(0 / 5)
+
+
+# 3) / ამიით გაყოფის დროს შედეგი გამოდის ათწილადი 
+# // ამით გაყოპის დროს კი შედეგი გამოდის მთელი რიცხვი
+
+# case-sensitive ში დიდი და პატარა ასოები განსხვავდება 
+# case-insensitive ში არ განსხვავდება
+
+name="toko"
+age=14
+city="zestafoni"
+car="alfa romeo"
+languge="python"
+
+print("my name is ", name, "am", age, "year", "i live", city, "my favorite car is", car, "i learn", languge, "." )
+
+
+
+num1=10
+num2=20
+num3=30
+
+print(num1 + num2 + num3)
