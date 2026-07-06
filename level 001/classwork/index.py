8dd6ee8eba749af109171fca675155e7604b44e2
toko04-studio <tokotoko4781@gmail.com>
2026-07-06 13:40:32 +0400

    second commit

---
+++ level 001/classwork/index.py
@@ -0,0 +1,19 @@
+
+print(5 + 3)   
+
+print(10 - 4) 
+
+print(6 * 2)  
+
+print(8 / 2)   
+
+print(7 % 3)   
+
+name = "Tornike"
+age = 14
+color = "black"
+car = "Alfa Romeo Giulia"
+city = "Zestaponi"
+
+print("My name is " + name + ", I am " + ("age") + " years old, my favorite color is " + color + ", my favorite car is the " + car + ", and I live in " + city + ".")
+
